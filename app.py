4d0e4616594b8776e8087eaca4dd915e0674066e
dannykan <dannykan@users.noreply.github.com>
2025-10-17 20:20:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -240,3 +240,14 @@ def health():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.getenv("PORT", "8000")), debug=True)
+
+
+from flask import send_from_directory
+
+@app.route("/")
+def serve_index():
+    return send_from_directory("static", "index.html")
+
+@app.route("/<path:path>")
+def serve_static(path):
+    return send_from_directory("static", path)
